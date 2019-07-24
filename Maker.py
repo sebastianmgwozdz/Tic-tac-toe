4ac6ab651ae845843c2e812f7408a7df2490964f
Sebastian <sebastianmgwozdz@gmail.com>
2019-07-23 20:02:41 -0700

    removed unneeded files

--- Maker.py
+++
@@ -1,5 +0,0 @@
-class Marker:
-    def __init__(self, player, column, row):
-        self.shape = player.shape
-        self.column = column
-        self.row = row
--- Player.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    def __init__(self, turnOrder, shape):
-        self.turnOrder = turnOrder
-        self.shape = shape
-
-
-
